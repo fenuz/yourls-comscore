a4e78df0ee2ce14a6a9f2ad47669e769a1366b4a
Frank Matheron <frankmatheron@gmail.com>
2013-06-20 13:15:15 +0200

    added configuration options
    - enable/disable use of cookies
    - http proxy host and port

---
+++ README.md
@@ -0,0 +1,43 @@
+Yourls-comscore plugin
+======================
+
+## Configuration
+
+The Yourls-comscore plugin is configured using defined constants in 
+`user/config.php`. The following options are available:
+
+ Name                     | Description 
+--------------------------|--------------------------------------------------------
+ COMSCORE_URL             | The ComScore tracking URL. This parameter is mandatory.
+ COMSCORE_USE_COOKIES     | TRUE to use cookies when visiting the tracking URL. Default: `true`.
+ COMSCORE_COOKIES_FILE    | Path to the file in which the cookies are stored. Default: `user/comscore.cookies`.
+ COMSCORE_HTTP_PROXY_HOST | Optional hostname for the http proxy that should be used to get the tracking URL.
+ COMSCORE_HTTP_PROXY_PORT | Optional port for the http proxy that should be used to get the tracking URL.
+
+These configuration options should be set in the YOURLS `user/config.php` file.
+
+### The COMSCORE_URL
+
+The COMSCORE_URL is the URL to the tracking image. The COMSCORE_URL is 
+formatted (using `sprintf()`) with two additional values: the yourls shorturl, 
+and the current `time()`.
+
+### Example configuration
+
+```php
+
+    // The ComScore tracking URL. %1$s is replaced with the shorturl, and 
+    // %2$s is replaced with the current time(stamp).
+    define('COMSCORE_URL', 'http://nl.sitestat.com/mycorp/mysite/s?page.%1$s&ns__t=%2$s');
+
+    // Enable the use of cookies, default: true.
+    // define('COMSCORE_USE_COOKIES', true);
+
+    // Path to the file in which the cookies are stored.
+    define('COMSCORE_COOKIES_FILE', '/path/to/the/cookies/file');
+
+    // Optional proxy settings.
+    define('COMSCORE_HTTP_PROXY_HOST', 'proxy.mydomain.com');
+    define('COMSCORE_HTTP_PROXY_PORT', '80');
+
+```
--- plugin.php
+++ plugin.php
@@ -8,6 +8,18 @@ Author: Frank Matheron <frankmatheron@gmail.com>
 Author URI: https://github.com/fenuz
 */
 
+if (!defined('COMSCORE_USE_COOKIES')) {
+    define('COMSCORE_USE_COOKIES', true);
+}
+
+if (!defined('COMSCORE_HTTP_PROXY_HOST')) {
+    define('COMSCORE_HTTP_PROXY_HOST', false);
+}
+
+if (!defined('COMSCORE_HTTP_PROXY_PORT')) {
+    define('COMSCORE_HTTP_PROXY_PORT', false);
+}
+
 if (defined('COMSCORE_URL')) {
 
     if (!defined('COMSCORE_COOKIE_FILE')) {
@@ -15,13 +27,30 @@ if (defined('COMSCORE_URL')) {
     }
     
     // Attempt to create a file to store the cookies in.
-    if (!file_exists(COMSCORE_COOKIE_FILE)) {
+    if (COMSCORE_USE_COOKIES && !file_exists(COMSCORE_COOKIE_FILE)) {
         if (touch(COMSCORE_COOKIE_FILE, time())){
             if (!chmod(COMSCORE_COOKIE_FILE, 0660)) {
                 error_log('Could not set read/write permissions to ComScore cookie file: ' . COMSCORE_COOKIE_FILE);
             }            
         } else {
             error_log('Could not create ComScore cookie file: ' . COMSCORE_COOKIE_FILE);
+        }
+    }
+    
+    function _comscore_copy_headers($ch) {
+        $headers = apache_request_headers();
+        if (array_key_exists('User-Agent', $headers)) {
+            curl_setopt($ch, CURLOPT_USERAGENT, $headers['User-Agent']);
+        }
+        if (array_key_exists('Referer', $headers)) {
+            curl_setopt($ch, CURLOPT_REFERER, $headers['Referer']);
+        }
+    }
+    
+    function _comscore_config_proxy($ch) {
+        if (COMSCORE_HTTP_PROXY_HOST && COMSCORE_HTTP_PROXY_PORT) {
+            curl_setopt($ch, CURLOPT_PROXY, COMSCORE_HTTP_PROXY_HOST);
+            curl_setopt($ch, CURLOPT_PROXYPORT, COMSCORE_HTTP_PROXY_PORT);
         }
     }
     
@@ -33,23 +62,29 @@ if (defined('COMSCORE_URL')) {
     function _comscore_log_redirect($args) {
         $url = sprintf(COMSCORE_URL, urlencode($args[1]), time());
         
-        $ch = curl_init($url);        
+        $ch = curl_init($url);     
+        _comscore_copy_headers($ch);
+        _comscore_config_proxy($ch);
+        
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         
         // ComScore uses redirects to track clients with cookies disabled, or 
         // for first time visits (when you do not have cookies yet).
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         
-        // We enable cookies. When we have a cookie stored ComScore no longer 
-        // redirects us, saving us a request.
-        if (is_writable(COMSCORE_COOKIE_FILE)) {
-            // We pretend that we never close our browser
-            curl_setopt($ch, CURLOPT_COOKIESESSION, false);
-            // Store the cookies in COMSCORE_COOKIE_FILE
-            curl_setopt($ch, CURLOPT_COOKIEFILE, COMSCORE_COOKIE_FILE);
-            curl_setopt($ch, CURLOPT_COOKIEJAR, COMSCORE_COOKIE_FILE);
-        } else {
-            error_log('Cannot write to ComScore cookie jar: ' . COMSCORE_COOKIE_FILE);
+        if (COMSCORE_USE_COOKIES) {
+            // We enable cookies. When we have a cookie stored ComScore no longer 
+            // redirects us, saving us a request.
+            if (is_writable(COMSCORE_COOKIE_FILE)) {
+                // We pretend that we never close our browser
+                curl_setopt($ch, CURLOPT_COOKIESESSION, false);
+                // Store the cookies in COMSCORE_COOKIE_FILE
+                curl_setopt($ch, CURLOPT_COOKIEFILE, COMSCORE_COOKIE_FILE);
+                curl_setopt($ch, CURLOPT_COOKIEJAR, COMSCORE_COOKIE_FILE);
+
+            } else {
+                error_log('Cannot write to ComScore cookie jar: ' . COMSCORE_COOKIE_FILE);
+            }
         }
         
         // Prevent requests taking too long
@@ -60,6 +95,7 @@ if (defined('COMSCORE_URL')) {
         if(curl_exec($ch) === false) {
             error_log('cURL error on ComScore request: ' . curl_error($ch));
         }
+
         curl_close($ch);
     }
     yourls_add_action('redirect_shorturl', '_comscore_log_redirect');
